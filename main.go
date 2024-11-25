474a34748ad28ebf680df050197c5ee1a39c45fd
MartinLinCW <130962723+martin-lin-cw@users.noreply.github.com>
2024-11-25 15:49:01 +0800

    add another say hello from 2 (#2)

--- main.go
+++ main.go
@@ -4,5 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World")
+<<<<<<< HEAD
 	fmt.Println("Hello World from say hello branch")
+=======
+	fmt.Println("Hello World from say hello 2 branch")
+>>>>>>> 19c67da (add another say hello from 2)
 }
